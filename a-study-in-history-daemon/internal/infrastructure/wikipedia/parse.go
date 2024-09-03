39145a6991b55e912a4918c51a04b1cb408a7cd1
Aaegamysta <Aaegamysta@protonmail.com>
2024-09-03 15:06:11 +0200

    added parseEvents and parseFirst page for customized parsing of wikipedia api response

---
+++ a-study-in-history-daemon/internal/infrastructure/wikipedia/parse.go
@@ -0,0 +1,81 @@
+package wikipedia
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/aaegamysta/a-study-in-history/spec/pkg/events"
+	"github.com/buger/jsonparser"
+)
+
+type firstPage struct {
+	description string
+	path string
+	width int64
+	height int64
+}
+
+func (i *Impl) parseEvents(_ context.Context, coll *events.EventsCollection, r io.ReadCloser) error {
+	bytes, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	bytes,dataType , offset, err := jsonparser.Get(bytes, coll.Type.String())
+	if err != nil {
+		return err
+	}
+	if dataType != jsonparser.Array {
+		return fmt.Errorf("error happened while parsing %s events on %d-%d: object parsed is not an array", coll.Type ,coll.Month, coll.Day)
+	}
+	if offset == 0 || bytes == nil {
+		return fmt.Errorf("error happened while parsing %s events on %d-%d: array is empty", coll.Type ,coll.Month, coll.Day)
+	}
+	offset, err = jsonparser.ArrayEach(bytes, func(b []byte, dataType jsonparser.ValueType, offset int, parsingErr error) {
+		title, err := jsonparser.GetString(b, "text")
+		if err != nil {
+			parsingErr = err
+			return
+		}
+		// not all events like holidays have no year
+		year, _ := jsonparser.GetInt(b, "year")
+		firstPage := parseFirstPage(b)
+		coll.Events = append(coll.Events, events.Event{
+			Type: coll.Type,
+			Day: coll.Day,
+			Month: coll.Month,
+			Year: year,
+			ID: "",
+			Title: title,
+			Description: firstPage.description,
+			Thumbnail: events.Thumbnail{
+				Path: firstPage.path,
+				Width: firstPage.width,
+				Height: firstPage.height,
+			},
+		})
+	})
+	if offset == 0 {
+		return fmt.Errorf("error happened while parsing %s events on %d-%d: array is empty", coll.Type ,coll.Month, coll.Day)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func parseFirstPage(bytes []byte) firstPage {
+	// first page and its contents extract and thumbnail might not present for some events
+	b, _, _, _ := jsonparser.Get(bytes, "pages", "[0]")
+	extract,  _ := jsonparser.GetString(b, "extract")
+	source, _ := jsonparser.GetString(b, "thumbnail", "source")
+	width, _ := jsonparser.GetInt(b, "thumbnail", "width")
+	height, _ := jsonparser.GetInt(b, "thumbnail", "height")
+
+	return firstPage{
+		description: extract,
+		path:        source,
+		width:      width,
+		height:      height,
+	}
+}
